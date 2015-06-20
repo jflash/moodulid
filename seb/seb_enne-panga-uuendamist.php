7e2f98592ab4d5dd0e4fb5d2fc3a453085662369
jflash <jflash@mail.ee>
2015-06-20 16:51:28 +0300

    1. SEB: nüüd pole bank.php faili enam vaja aga veel jätan alles. 2. SEB: Koodid panka andmete saatmiseks ja vastuvõtmiseks on uuendatud ja vajalikult ka täiendatud. Ainult et funktisoon signature_check annab nüüd tulemuseks 0 ehk nagu poleks korras. Samas panga poolt kõik liigub. Tegin sellesse funktsiooni enda jaoks debugi, ehk Joomla root kausta tuleb luu järgnevad viis tekstifaili mis realt 393 algavad...

--- seb/seb_enne-panga-uuendamist.php
+++ seb/seb_enne-panga-uuendamist.php
@@ -47,7 +47,7 @@ class plgVmPaymentseb extends vmPSPlugin {
     	$SQLfields = array(
     	    'id' => 'int(11) unsigned NOT NULL AUTO_INCREMENT',
     	    'virtuemart_order_id' => 'int(11) UNSIGNED DEFAULT NULL',
-			'order_number'			=> 'char(64)',
+    	    'order_number' => 'char(32) DEFAULT NULL',
     	    'virtuemart_paymentmethod_id' => 'mediumint(1) UNSIGNED DEFAULT NULL',
     	    'payment_name' => 'char(255) NOT NULL DEFAULT \'\' ',
     	    'payment_order_total' => 'decimal(15,5) NOT NULL DEFAULT \'0.00000\' ',
@@ -77,7 +77,7 @@ VmConfig::loadJLang('com_virtuemart_orders', TRUE);
     	    require(VMPATH_ADMIN . DS . 'models' . DS . 'orders.php');
     	$this->getPaymentCurrency($method);
    	    $q = 'SELECT `currency_code_3` FROM `#__virtuemart_currencies` WHERE `virtuemart_currency_id`="' . $method->payment_currency . '" ';
-    	$db = JFactory::getDBO();
+    	$db = &JFactory::getDBO();
     	$db->setQuery($q);
     	$currency_code_3 = $db->loadResult();
     	$paymentCurrency = CurrencyDisplay::getInstance($method->payment_currency);
@@ -115,7 +115,7 @@ VmConfig::loadJLang('com_virtuemart_orders', TRUE);
         $paymentCurrency = CurrencyDisplay::getInstance($method->payment_currency);
     	$totalInPaymentCurrency = round($paymentCurrency->convertCurrencyTo($method->payment_currency, $order['details']['BT']->order_total,false), 2);
         $bank_order_id= VirtueMartModelOrders::getOrderIdByOrderNumber($order['details']['BT']->order_number);
-        $VK_a['VK_SERVICE'] = '1012';
+        $VK_a['VK_SERVICE'] = '1002';
         $VK_a['VK_VERSION'] = '008';
         $VK_a['VK_SND_ID'] = $dbValues['VK_SND_ID'];
         $VK_a['VK_STAMP'] = hexdec($bank_order_id);
@@ -123,16 +123,13 @@ VmConfig::loadJLang('com_virtuemart_orders', TRUE);
         $VK_a['VK_CURR'] = 'EUR';
         $VK_a['VK_REF'] = $this->generateRefNum($bank_order_id);
         $VK_a['VK_MSG'] = "Arve ". $bank_order_id. " tasumine";
-        //$VK_a['VK_RETURN'] = JROUTE::_(JURI::root() . 'plugins/vmpayment/seb/bank.php');
-        $VK_a['VK_RETURN'] = self::getSuccessUrl($order);
-        //$VK_a['VK_CANCEL'] = $VK_a['VK_RETURN'];
-        $VK_a['VK_CANCEL'] = self::getCancelUrl($order);
-        $vkDateTime = new DateTime('NOW');
-        $VK_a['VK_DATETIME'] = $vkDateTime->format(DateTime::ISO8601); //date("Y-m-d H:i:s", time()); //ISO-8601 
         $VK_a['VK_MAC'] = $this->createSignature($VK_a,$dbValues['priv_key'],$dbValues['priv_pass']);
-        $VK_a['VK_ENCODING'] = 'UTF-8';
-        $VK_a['VK_LANG'] = 'EST';
-      //$VK_a['VK_ACC'] = '10002050618003'; //ainult SEB TEST-versiooni jaoks, aga pole vist üldse vaja
+        $VK_a['VK_RETURN'] = JROUTE::_(JURI::root() . 'plugins/vmpayment/seb/bank.php');
+        $VK_a['VK_CANCEL'] = $VK_a['VK_RETURN'];
+
+
+
+
         $html.=( '<form method="post" action="'.$dbValues['url'].'">' );
         foreach( $VK_a as $VK_name => $VK_value ){
            $html.=( '<input type="hidden" name="' . htmlspecialchars($VK_name) . '" value="' . htmlspecialchars($VK_value) . '"/>'."\n"."\t" );
@@ -140,7 +137,6 @@ VmConfig::loadJLang('com_virtuemart_orders', TRUE);
         $html.=("</br>");
         $html.= ('<input type="submit" value="'.JText::_("VMPAYMENT_SEB_SUBMIT").'"/>' );
         $html.=( '</form>' );
-		        $cart->emptyCart (); //teistel oli ka ja see pangavalikul kui lehte refreshitakse kustutab ostukaartilt kõik tooted
         		vRequest::setVar ('html', $html);
 return TRUE;
 //    	return $this->processConfirmedOrderPaymentResponse(true, $cart, $order, $html, , $method->status_canceled);
@@ -149,17 +145,17 @@ return TRUE;
    	if (!$this->selectedThisByMethodId($virtuemart_payment_id)) {
     	    return null; // Another method was selected, do nothing
     	}
-      //VmConfig::loadJLang('com_virtuemart');
+      VmConfig::loadJLang('com_virtuemart');
     	$db = JFactory::getDBO();
     	$q = 'SELECT * FROM `' . $this->_tablename . '` '
     		. 'WHERE `virtuemart_order_id` = ' . $virtuemart_order_id;
     	$db->setQuery($q);
     	if (!($paymentTable = $db->loadObject())) {
-//     	    vmWarn(500, $q . " " . $db->getErrorMsg());
-//     	    return '';
+    	    vmWarn(500, $q . " " . $db->getErrorMsg());
+    	    return '';
     	}
     	$this->getPaymentCurrency($paymentTable);
-    	$html = '<table class="adminlist table">' . "\n";
+    	$html = '<table class="adminlist">' . "\n";
     	$html .=$this->getHtmlHeaderBE();
     	$html .= $this->getHtmlRowBE('SEB_PAYMENT_NAME', $paymentTable->payment_name);
     	$html .= '</table>' . "\n";
@@ -189,7 +185,7 @@ return TRUE;
     	}
     	 $this->getPaymentCurrency($method);
     	$paymentCurrencyId = $method->payment_currency;
-      return; //teistel pole
+      return;
     }
   function plgVmOnCheckAutomaticSelectedPayment (VirtueMartCart $cart, array $cart_prices = array(), &$paymentCounter) {
 		return $this->onCheckAutomaticSelected ($cart, $cart_prices, $paymentCounter);
@@ -200,7 +196,7 @@ return TRUE;
     function plgVmonShowOrderPrintPayment($order_number, $method_id) {
 	   return $this->onShowOrderPrint($order_number, $method_id);
     }
-    function plgVmDeclarePluginParamsPayment($name, $id, &$data) { //VM3 korral pole seda siia vaja
+    function plgVmDeclarePluginParamsPayment($name, $id, &$data) {
 	   return $this->declarePluginParams('payment', $name, $id, $data);
     }
   function plgVmDeclarePluginParamsPaymentVM3( &$data) {
@@ -230,11 +226,9 @@ return TRUE;
     function plgVmOnPaymentResponseReceived(&$html) {
         //language
         if (!class_exists('VirtueMartCart'))
-            require(VMPATH_SITE . DS . 'helpers' . DS . 'cart.php');
-		if (!class_exists ('shopFunctionsF')) {
-			require(VMPATH_SITE . DS . 'helpers' . DS . 'shopfunctionsf.php'); }
+            require(JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php');
        	if (!class_exists('VirtueMartModelOrders'))
-    	    require( VMPATH_ADMIN . DS . 'models' . DS . 'orders.php' );    
+    	    require( JPATH_VM_ADMINISTRATOR . DS . 'models' . DS . 'orders.php' );    
         $lang = JFactory::getLanguage();
     	$filename = 'com_virtuemart';
     	$lang->load($filename, JPATH_ADMINISTRATOR);
@@ -243,18 +237,12 @@ return TRUE;
         $order_id = substr($payment_data['VK_REF'], 0, -1);
         $pmethod=VirtueMartModelOrders::getOrder($order_id);
         $payment_method= $pmethod['details']['BT']->virtuemart_paymentmethod_id;
-    	//if (!($this->_currentMethod = $this->getVmPluginMethod($payment_method))) { //korjan ringi
-      if (!($method = $this->getVmPluginMethod($payment_method))) {
+    	if (!($method = $this->getVmPluginMethod($payment_method))) {
     	    return null; // Another method was selected, do nothing
     	}
-    	//if (!$this->selectedThisElement($this->_currentMethod->payment_element)) {  //korjan ringi
-      if (!$this->selectedThisElement($method->payment_element)) {
-    	    return null; //oli false
+    	if (!$this->selectedThisElement($method->payment_element)) {
+    	    return false;
     	}
-
-// 		if (!($virtuemart_order_id = VirtueMartModelOrders::getOrderIdByOrderNumber($order_number))) { //lisasin selle ka kuna teistel on //korjan ringi
-// 			return FALSE;
-// 		}
     	
                 
     	$payment_name = $this->renderPluginName($method);
@@ -267,15 +255,14 @@ return TRUE;
         $sig_result = $this->signature_check($vk_array,$method->pub_key );
         $html = '<table>' . "\n";
             if($sig_result==0)
-                $html .= $this->getHtmlRow('SEB_BANK_ERROR'," ").$sig_result;
+                $html .= $this->getHtmlRow(SEB_BANK_ERROR," ");
             $html .= '</table>' . "\n";
-            //$config = JFactory::getConfig();
-            //$dbprefix =   $config->getValue( 'dbprefix' );
-            $dbprefix = VmConfig::get('dbprefix',false); //oih ma ei tea kas eelmise kahe rea asemel nii sobib
+            $config =& JFactory::getConfig();
+            $dbprefix =   $config->getValue( 'dbprefix' );
              $order= new VirtueMartModelOrders();
              $order=$order->getOrder($order_id);
              $cart = VirtueMartCart::getCart();
-            if($_REQUEST['VK_SERVICE']==1111 && $sig_result==1){
+            if($_REQUEST['VK_SERVICE']==1101 && $sig_result==1){
             $html .= $this->_getPaymentResponseHtml($payment_data, $payment_name);
                      if(isset($cart->_dataValidated)){
                          if($order['details']['BT']->order_status!=$method->status_success){
@@ -283,7 +270,7 @@ return TRUE;
                         }
                         echo JText::_("VMPAYMENT_SEB_SUCCESS_MESSAGE");
                     }
-            }elseif($_REQUEST['VK_SERVICE']==1911 && $sig_result==1){// makse on tühistatud
+            }elseif($_REQUEST['VK_SERVICE']==1901 && $sig_result==1){// makse on tühistatud
                 if($order['details']['BT']->order_status!=$method->status_canceled){
             	   $this->update_status($method->status_canceled,$order_id,"VMPAYMENT_SEB_PAYMENT_CANCELED");
                 }
@@ -292,9 +279,7 @@ return TRUE;
             $cart->emptyCart();
             $url=JROUTE::_(JURI::root() .'index.php?option=com_virtuemart&view=orders');
             echo '<head><meta http-equiv="Refresh" content="1;URL='.$url.'"></head>';
-//exit;
-$cart = VirtueMartCart::getCart ();
-$cart->emptyCart ();
+exit;
             return true;
    }
     protected function checkConditions($cart, $method, $cart_prices) {
@@ -320,11 +305,7 @@ $cart->emptyCart ();
         if (count($countries) == 0 || in_array($address['virtuemart_country_id'], $countries) || count($countries) == 0) {
             return true;
         }
-// 		if (in_array($address['virtuemart_country_id'], $countries) || count($countries) == 0) { //teistel on eelmine lõik nii, meil sellega ei toimi
-// 			if ($amount_cond) {
-// 				return TRUE;
-// 			}
-// 		}
+
         return false;
     }
     function update_status($status,$order_id,$text='VMPAYMENT_SEB_PAYMENT_CANCELED'){
@@ -351,24 +332,23 @@ $cart->emptyCart ();
     }
     function generateMACString($macFields) {
 		$VK_variableOrder = Array(
-		    1011 => Array(
+		1001 => Array(
 				'VK_SERVICE','VK_VERSION','VK_SND_ID',
 				'VK_STAMP','VK_AMOUNT','VK_CURR',
 				'VK_ACC','VK_NAME','VK_REF','VK_MSG'
-        //,'VK_RETURN', 'VK_CANCEL', 'VK_DATETIME' //ise lisatud
 				),
-				1012 => Array(
+				1002 => Array(
 				'VK_SERVICE','VK_VERSION','VK_SND_ID',
 				'VK_STAMP','VK_AMOUNT','VK_CURR',
 				'VK_REF','VK_MSG'
 				),
-				1111 => Array(
+				1101 => Array(
 				'VK_SERVICE','VK_VERSION','VK_SND_ID',
 				'VK_REC_ID','VK_STAMP','VK_T_NO','VK_AMOUNT','VK_CURR',
 				'VK_REC_ACC','VK_REC_NAME','VK_SND_ACC','VK_SND_NAME',
-				'VK_REF','VK_MSG','VK_T_DATETIME'
+				'VK_REF','VK_MSG','VK_T_DATE'
 				),
-				1911 => Array(
+				1901 => Array(
 				'VK_SERVICE','VK_VERSION','VK_SND_ID',
 				'VK_REC_ID','VK_STAMP','VK_REF','VK_MSG'
 				),
@@ -386,19 +366,13 @@ $cart->emptyCart ();
         $signature = base64_decode( $VK_MAC );
         $data=$this->generateMACString($VK_a);
         $pubkey = openssl_get_publickey( $pub_key );
-        //uuem$pubkey = openssl_pkey_get_public( $pub_key );
         $out = openssl_verify( $data, $signature, $pubkey );
         openssl_free_key( $pubkey );
-        file_put_contents("001vkmac.txt", $VK_MAC.PHP_EOL, FILE_APPEND); //Joomla root kausta tuleb luu järgnevad viis tekstifaili
-        file_put_contents("002sign.txt", $signature.PHP_EOL, FILE_APPEND);
-        file_put_contents("003mac.txt", $data.PHP_EOL, FILE_APPEND);
-        file_put_contents("004pubkey.txt", $pubkey.PHP_EOL, FILE_APPEND);
-        file_put_contents("005out.txt", $out.PHP_EOL, FILE_APPEND);
         return $out;
     }
 	function generateRefNum($stamp) {
 		$chcs = array(7, 3, 1);
-		$sum = 0;
+    $sum = 0;
 		$pos = 0;
 		for ($i = 0; $i < strlen($stamp); $i++) {
 			$x = (int)(substr($stamp,strlen($stamp) - 1 - $i, 1));
@@ -417,47 +391,4 @@ $cart->emptyCart ();
 		}
 		return $stamp . $sum;
 	}
-  //siia lisan nüüd neid mida polnud
-	function plgVmOnConfirmedOrderStorePaymentData ($virtuemart_order_id, $orderData, $priceData) {
-		if (!$this->selectedThisPayment ($this->_pelement, $orderData->virtuemart_paymentmethod_id)) {
-			return NULL; // Another method was selected, do nothing
-		}
-		return FALSE;
-	}
-
-	public function plgVmOnUpdateOrderPayment ($_formData) {
-		return NULL;
-	}
-
-	public function plgVmOnUpdateOrderLine ($_formData) {
-		return NULL;
-	}
-
-	public function plgVmOnEditOrderLineBE ($_orderId, $_lineId) {
-		return NULL;
-	}
-
-	public function plgVmOnShowOrderLineFE ($_orderId, $_lineId) {
-		return NULL;
-	}
-
-	protected function getLang () {
-		$language =& JFactory::getLanguage ();
-		$tag = strtolower (substr ($language->get ('tag'), 0, 2));
-		return $tag;
-	}
-
-// sofort
-	static function   getSuccessUrl ($order) {
-		return JURI::root()."index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&pm=" . $order['details']['BT']->virtuemart_paymentmethod_id . '&on=' . $order['details']['BT']->order_number . "&Itemid=" . vRequest::getInt('Itemid'). '&lang='.vRequest::getCmd('lang',''); ;
-    //http://www.disalena.com/vm3/index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&pm=4&on=39d1024&Itemid=124&lang=
-	}
-	static function   getCancelUrl ($order) {
-		return  JURI::root()."index.php?option=com_virtuemart&view=pluginresponse&task=pluginUserPaymentCancel&pm=" . $order['details']['BT']->virtuemart_paymentmethod_id . '&on=' . $order['details']['BT']->order_number . '&Itemid=' . vRequest::getInt('Itemid').'&lang='.vRequest::getCmd('lang','');
-    //http://www.disalena.com/vm3/index.php?option=com_virtuemart&view=pluginresponse&task=pluginUserPaymentCancel&pm=4&on=3982026&Itemid=124&lang=
-	}
-	static function   getNotificationUrl ($security, $order_number) {
-		return JURI::root()  .  "index.php?option=com_virtuemart&view=pluginresponse&task=pluginnotification&tmpl=component&&security=" . $security . "&on=" . $order_number .'&lang='.vRequest::getCmd('lang','');
-	}
-
 }
